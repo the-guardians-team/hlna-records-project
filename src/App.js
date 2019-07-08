9326bb8d8105de13c4f8ddbc6530e27dc7c66f7f
LamarZM <lamar.mufti@yahoo.com>
2019-07-08 12:47:14 +0300

    Created a search event for title Created a search event for artist(needs adjustment)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-// import axios from 'axios'
+import SearchByTitle from './SearchByTitle'
 // import components
 import Artist from './Artist'
 
@@ -10,9 +10,8 @@ class App extends Component {
     //  console.log(this.state.artist)
     return (
       <div className="App">
-        <Artist />
-        {/* <SearchByTitle />
-        <SlideShow />
+         <SearchByTitle />
+        {/* <SlideShow />
         <Track /> */}
         <div className="card">
           {/* {this.state.artist.map((artist, index) =>(
@@ -23,47 +22,11 @@ class App extends Component {
            )
       )} */}
         </div>
-      </div>
+        </div>
+
+
     );
   }
 }
 
 export default App;
-
-
-
-        /////////////END /////
-    // API calls
-    // axios({
-    //   method: "get",
-    //   url:
-    //     "https://cors-anywhere.herokuapp.com/http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=kuwait&api_key=36439381aa28fa5ef0af0232bb7a274f&format=json"
-    // })
-    //   .then(response => {
-    //      console.log(response.data.topartists)
-    //     // console.log(response.data.topartists.artist);
-    //     const topartists = response.data.topartists.artist;
-    //     let count = -1;
-    //     const minTopArtists = topartists.filter(artist => {
-    //       count++;
-    //       return count < topartists.length / 2;
-    //     });
-    //     // console.log(minTopArtists);
-    //     this.setState({ minTopArtists });
-    //   })
-    //   .catch(error => {});
-
-
-    
-// const callToken =()=> {
-//   let parameters = {"client_id" : "ff441d52896a41bbb9b634baa3f80c03",// u get in developer account in Spotify.
-//   "client_secret" : "a132da2b8cc84305b0b0ef95cebaa3c0",
-//   "grant_type" : "client_credentials"}
-//   axios.post("https://accounts.spotify.com/api/token", parameters)
-//     .then(response =>{
-//         console.log(response)
-              
-//     })
-                          
-
-//     }  
---
+++ src/SearchByTitle.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import axios from 'axios'
+
+class SearchByTitle extends Component {
+
+  state = {
+    tracks: [],
+    formData: {
+      title: '',
+      artist:''
+    }
+  }
+
+  handleChange = event => {
+    //get the user's input
+    const userInput = event.target.value;
+    //get which input they typed in
+    const name = event.target.name;
+    //copy the state of formData
+    const newFormData = Object.assign(this.state.formData);
+    //update the key in formData copy with the new value
+    newFormData[name] = userInput;
+    //set the state with the updated formData
+    this.setState({
+      formData: newFormData
+    });
+  };
+  // state ={ key:json file, key:json file, ...}
+
+  
+  
+  componentDidMount() {
+    // API calls
+    // axios({
+    // method: "get", 
+    // url: 'https://cors-anywhere.herokuapp.com/https://ws.audioscrobbler.com//2.0/?method=track.search&track=Believe&api_key=fa6d7c53999a32c59ed71bb3d371ea07&format=json'
+
+    //  })
+
+    // .then(res => {
+      // console.log(res);
+    //   this.setState({ tracks: res.data.results.trackmatches.track});
+    // }
+    // )
+  }
+  handleSubmitArtist = event => {
+    
+    //prevent default action
+    event.preventDefault();
+
+
+  
+  }
+  handleSubmitTitle = event => {
+    this.setState({event: this.state.formData.title})
+      //prevent default action
+      event.preventDefault();
+  
+  
+    //get the formData
+    const title = this.state.formData.title
+    axios({
+      method: "get", 
+      url: `https://cors-anywhere.herokuapp.com/https://ws.audioscrobbler.com//2.0/?method=track.search&track=${title}&api_key=fa6d7c53999a32c59ed71bb3d371ea07&format=json`
+  
+       })
+  
+      .then(response =>{
+        console.log(response);
+
+        this.setState({
+        tracks: response.data.results.trackmatches.track
+        })
+      })
+
+      .catch()
+  }
+
+  render() {
+    return( 
+
+      <div className="App">
+        <ul>{this.state.tracks.map((track, index) => <li key={index}>{track.name}</li>)}
+        <ul>{this.state.tracks.map((track, index) => <li key={index}>{track.artist}</li>)}</ul>
+        </ul>
+        <form onSubmit={this.handleSubmitTitle}>
+          <input
+            placeholder="Search by title"
+            name="title"
+            value={this.state.formData.title}
+            onChange={this.handleChange}
+          />
+        </form>
+        <form onSubmit={this.handleSubmitArtist}>
+          <input
+            placeholder="Search by artist"
+            name="artist"
+            value={this.state.formData.artist}
+            onChange={this.handleChange}
+          />
+        </form>
+
+
+      </div>
+    );
+  }
+}
+
+
+export default SearchByTitle;
