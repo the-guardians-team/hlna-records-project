4cb86e704c7c8b544d521b3691cf351469a94f2d
akhouja1994 <a.khouja1994@gmail.com>
2019-07-08 15:48:17 +0300

    toptrack with rebasing

--- src/App.js
+++ src/App.js
@@ -11,28 +11,13 @@ import axios from "axios";
 
 class App extends Component {
   // state ={ key:json file, key:json file, ...}
-  state = {
-    top50Tracks:[]
-  }
-  componentDidMount() {
-    // API calls
-    axios({
-      method: 'get',
-      url: `https://www.theaudiodb.com/api/v1/json/1/mostloved.php?format=track`
-    })
-    .then(response => {
-      console.log(response)
-      const allTrack = response.data.loved
-      this.setState({
-        top50Tracks: allTrack
-      })
-    })
-  }
+  
   render() {
     //  console.log(this.state.artist)
     return (
       <div className="App">
          <SearchByTitle />
+         <TopTrack />
         {/* <SlideShow />
         <Track /> */}
         <div className="card">
@@ -44,13 +29,7 @@ class App extends Component {
            )
       )} */}
         </div>
-        {/* <SearchByTitle /> */}
-        {/* <SlideShow /> */}
-        {this.state.top50Tracks.map(loved => <TopTrack key={loved.idTrack} 
-        tId={loved.idTrack} arId={loved.idArtist} alId={loved.idAlbum} 
-        tName={loved.strTrack} arName={loved.strArtist} alName={loved.strAlbum} 
-        arImg={loved.strTrackThumb} tVid={loved.strMusicVid} />)}
-        {/* artist component will contain <Album /> , and album componenet will contain <Track /> */}
+
       </div>
     );
   }
--- src/TopTrack.js
+++ src/TopTrack.js
@@ -1,11 +1,34 @@
 import React,{Component} from 'react'
+import axios from 'axios'
+
 
 class TopTrack extends Component{
+    state = {
+        top50Tracks:[]
+      }
+      componentDidMount() {
+        // API calls
+        axios({
+          method: 'get',
+          url: `https://www.theaudiodb.com/api/v1/json/1/mostloved.php?format=track`
+        })
+        .then(response => {
+          const allTrack = response.data.loved
+          this.setState({
+            top50Tracks: allTrack
+          })
+        })
+      }
     render(){
         return(
             <div>
-                <h1>{this.props.tName}</h1>
-                <img src={this.props.arImg}/>
+                {this.state.top50Tracks.map(loved =>
+                    <div>
+                        <h1>{loved.strTrack}</h1>
+                        <img src={loved.strTrackThumb}/>
+                    </div>
+                )}
+                
             </div>
         )
     }
