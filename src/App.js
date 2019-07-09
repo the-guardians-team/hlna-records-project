1770befed20005f53510511f7bd3fdf154f171b5
hamoghamdi <haneen.aljabrah@gmail.com>
2019-07-09 14:44:58 +0300

    link & routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import { Route, Link } from "react-router-dom";
 // import components
 import Search from './Search'
 import TopAlbums from './TopAlbums'
 import TopTracks from './TopTracks'
+import Track from "./Track";
+
 
 class App extends Component {
 
   render() {
-    return (
+
+    const Main = ()=>(
       <div className="App">
         <Search />
         {/* the form must be always on the top of the page 'All pages', BUT on submit it should direct to 'Search Results Page' */}
         <div className="card">
-        {/* rendered in this page 'Home Page' */}
+          {/* rendered in this page 'Home Page' */}
           <h1>Top Tracks</h1>
+          {/* <Route exact path="/" component={App} /> */}
+          
           {/* 
           when you click on a track it directs you to the 'Track Page'
            */}
@@ -27,6 +32,12 @@ class App extends Component {
           <TopAlbums />
         </div>
       </div>
+    )
+    return (
+      <React.Fragment>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/track/:id" component={Track} />
+      </React.Fragment>
     );
       
   }
--- src/Track.js
+++ src/Track.js
@@ -1,30 +1,33 @@
 import React,{Component} from 'react'
 import axios from 'axios'
-
+// import {Link} from "react-router-dom";
 class Track extends Component {
-    state={
-        track:''
-    }
-    componentDidMount(){
-        axios({
-            method: 'get',
-            url:`https://theaudiodb.com/api/v1/json/195187/track-mb.php?i=${this.props.tId}`
-        })
-        .then(response => {
-            console.log(response)
-            this.setState({
-                track: response.data.track[0]})
-        })
-    }
-    render(){
-        return(
-            <div>
-                <p>
+  state = {
+    track: ""
+  };
+  componentDidMount(){
+      axios({
+          method: 'get',
+          url:`https://theaudiodb.com/api/v1/json/195187/track-mb.php?i=${this.props.match.params.id}`
+      })
+      .then(response => {
+          console.log(response)
+          this.setState({
+              track: response.data.track[0]})
+      })
+  }
+  render() {
+    return (
+      <div>
+        <h1>this is track page</h1>
+        {/* {this.props.match.params.id} */}
+        <p>
                    {this.state.track.strTrack} 
                 </p>
-                <img src={this.state.track.strTrackThumb}/>
-            </div>
-        )
-    }
+                
+                <img src={this.state.track.strTrackThumb} alt=""/>
+      </div>
+    );
+  }
 }
 export default Track;
