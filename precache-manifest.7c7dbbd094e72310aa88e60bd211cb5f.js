13fb018e9e61723f3a84805b98f38f9db9ddd4b6
hamoghamdi <haneen.aljabrah@gmail.com>
2019-07-10 17:19:56 +0300

    Updates

---
+++ precache-manifest.7c7dbbd094e72310aa88e60bd211cb5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f69bf6218ca0b434946405b5146649",
+    "url": "/hlna-records-project/index.html"
+  },
+  {
+    "revision": "44b753fc4604002171f0",
+    "url": "/hlna-records-project/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "126405a37ccc44112502",
+    "url": "/hlna-records-project/static/css/main.7970b391.chunk.css"
+  },
+  {
+    "revision": "44b753fc4604002171f0",
+    "url": "/hlna-records-project/static/js/2.3bd55b67.chunk.js"
+  },
+  {
+    "revision": "126405a37ccc44112502",
+    "url": "/hlna-records-project/static/js/main.3437e6a6.chunk.js"
+  },
+  {
+    "revision": "9666fa6fba9e5780f5e2",
+    "url": "/hlna-records-project/static/js/runtime~main.15379873.js"
+  },
+  {
+    "revision": "f925f5c9ff141881cd6caa62848d496e",
+    "url": "/hlna-records-project/static/media/imageheader.f925f5c9.jpg"
+  }
+]);
